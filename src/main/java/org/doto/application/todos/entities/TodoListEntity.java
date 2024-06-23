9919b2605115d0b7b286166e672d288ab3a250fe
dossancto <dossantossilvalucas963@gmail.com>
2024-06-22 22:15:23 -0300

    [fix] todo repository

--- src/main/java/org/doto/application/todos/entities/TodoListEntity.java
+++ src/main/java/org/doto/application/todos/entities/TodoListEntity.java
@@ -1,13 +1,22 @@
 package org.doto.application.todos.entities;
 import java.time.LocalDateTime;
 import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import org.doto.application.users.entities.UserEntity;
 
 
 
 @Data
+@Entity
+@Table(name = "todos")
 public class TodoListEntity {
+    @Id
+    @GeneratedValue
     private UUID id;
     private String name;
 
--- src/main/java/org/doto/infra/repositories/todos/MMTodoListRepository.java
+++ src/main/java/org/doto/infra/repositories/todos/MMTodoListRepository.java
@@ -1,43 +1,18 @@
 package org.doto.infra.repositories.todos;
 
-import jakarta.inject.Singleton;
-import java.util.ArrayList;
-import java.util.List;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+
 import java.util.UUID;
 import org.doto.application.todos.data.TodoListRepository;
 import org.doto.application.todos.entities.TodoListEntity;
-import org.doto.application.users.data.UserRepository;
-import org.doto.infra.repositories.users.MMUserRepository;
-import org.doto.domain.exceptions.NotFoundException;
 
-class InMemoryDatabase{
-    public List<TodoListEntity> TodoLists = new ArrayList<>();
 
-    public void add(TodoListEntity list){
-        TodoLists.add(list);
-    }
-}
-
-@Singleton
-public class MMTodoListRepository implements TodoListRepository {
-    InMemoryDatabase database = new InMemoryDatabase();
-
-    final UserRepository _userRepository;
-
-    public MMTodoListRepository(UserRepository userRepository){
-        _userRepository = userRepository;
-    }
-
+@ApplicationScoped
+public class MMTodoListRepository implements TodoListRepository, PanacheRepositoryBase<TodoListEntity, UUID> {
     @Override
     public TodoListEntity create(TodoListEntity list) {
-        list.setId(UUID.randomUUID());
-
-        if(!_userRepository.findById(list.getUserId()).isPresent()){
-            throw new NotFoundException("Can't find user.");
-        }
-
-        database.add(list);
-
+        this.persist(list);
         return list;
     }
 }
